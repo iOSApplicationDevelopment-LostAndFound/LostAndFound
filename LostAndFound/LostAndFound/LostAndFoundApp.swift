0b17eb1f32cdac551adaf9f28a3e36eab07c4bde
Daniel You <onikss793@gmail.com>
2026-05-02 06:21:58 +0000

    init: add Xcode project scaffold for LostAndFound iOS app

---
+++ LostAndFound/LostAndFound/LostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndFoundApp.swift
+//  LostAndFound
+//
+//  Created by Daniel You on 2/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct LostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
